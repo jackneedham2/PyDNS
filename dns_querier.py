8e21b8549ab714001cbb76fafd7d6646ec21b9ef
jackneedham2 <jackpneedham@gmail.com>
2026-08-25 09:01:06 +0100

    Update dns_querier.py

--- dns_querier.py
+++ dns_querier.py
@@ -1,41 +1,44 @@
 import socket
 
-def string_splitter(s):
-    spl = s.split(".")
-    outbytes = []
-    for section in spl:
-        outbytes += [len(section)]
-        for c in section:
-            outbytes += [ord(c)]
-    return outbytes
-
-# One-Shot Multicast DNS Querier.
-# Standard DNS packet sent to 244.0.0.251:5353
-domain = "_nmos-node._tcp.local"
-
-packet = [
-
-# Header
-
-0,0, # Transaction ID
-1,0, # Flags
-0,1, # Num questions
-0,0, # Num Answer RRs
-0,0, # Num Authority RRs
-0,0, # Num Additional RRs
-
+class DNSQuerier:
+    header = [
+        0,0, # Transaction ID
+        1,0, # Flags
+        0,1, # Num questions
+        0,0, # Num Answer RRs
+        0,0, # Num Authority RRs
+        0,0, # Num Additional RRs
     ]
+    def __init__(self):
+        pass
 
-# Add query to end
+    def format_domain(self, s):
+        spl = s.split(".")
+        outbytes = []
+        for section in spl:
+            outbytes += [len(section)]
+            for c in section:
+                outbytes += [ord(c)]
+        return outbytes
 
-packet += string_splitter("yahoo.net")
-packet += [0,0]
-packet += [1, 0] # Type: PTR Record
-packet += [1, 0] # QCLASS.
-packet += [0x80, 0x1]
+    def create_query_packet(self, d):
+        packet = []
+        packet += self.header
+        packet += self.format_domain(d)
+        packet += [0,0]
+        packet += [1, 0] # Type: PTR Record
+        packet += [1, 0] # QCLASS = IN
+        packet += [0x80, 0x1]
+        return packet
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(bytes(packet), ("1.1.1.1",53))
-d = s.recvfrom(1024)[0]
+    def query_domain(self,d, ip, port):
+        packet = self.create_query_packet(d)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.sendto(bytes(packet), (ip,port))
+        d = s.recvfrom(1024)
+        return d
 
-print(d)
+
+
+my_querier = DNSQuerier()
+print(my_querier.query_domain("google.com","1.1.1.1",53))
